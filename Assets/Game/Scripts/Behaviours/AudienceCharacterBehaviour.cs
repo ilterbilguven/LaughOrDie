9ab67dd620aad0d70d581754149cce4b1e1e6466
Buğra Güler <hbguler@gmail.com>
2024-01-28 10:27:33 +0300

    Audience animations added

--- Assets/Game/Scripts/Behaviours/AudienceCharacterBehaviour.cs
+++ Assets/Game/Scripts/Behaviours/AudienceCharacterBehaviour.cs
@@ -5,14 +5,18 @@ namespace Game.Scripts.Behaviours
     public class AudienceCharacterBehaviour : MonoBehaviour
     {
         [SerializeField] private Animator animator;
-        private int totalAnimCount = 2;
+        private int totalAnimCount = 5;
+        private static int animIndex = 2;
 
         public void Start()
         {
-            int randomAnimIndex = Random.Range(1, totalAnimCount + 1);
-            animator.SetTrigger("Anim" + randomAnimIndex);
-            
-            if(randomAnimIndex == 1)
+            //int randomAnimIndex = Random.Range(1, totalAnimCount + 1);
+            animator.SetTrigger("Anim" + (animIndex + 1));
+            animator.Update(Random.Range(0, 18));
+
+            animIndex = (animIndex + 1) % totalAnimCount;
+
+            if (animIndex == 1)
                 transform.Rotate(0, 180f, 0f);
         }
     }
