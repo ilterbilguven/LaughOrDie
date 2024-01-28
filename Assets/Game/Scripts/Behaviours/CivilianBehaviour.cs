e50803ad9dff8d7844dbea8cfd37cc3e8b7e1d77
Buğra Güler <hbguler@gmail.com>
2024-01-28 05:01:36 +0300

    Random civilians running

--- Assets/Game/Scripts/Behaviours/CivilianBehaviour.cs
+++ Assets/Game/Scripts/Behaviours/CivilianBehaviour.cs
@@ -1,18 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
+public enum CivilianState
+{
+    Idle,
+    Moving,
+    Dead
+}
 
 public class CivilianBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Animator animator;
+    [SerializeField] private NavMeshAgent agent;
+    private Coroutine _movementCoroutine;
+    private CivilianState _state;
+
+    public void StartMovement()
     {
-        
+        animator.SetTrigger("Run");
+        _movementCoroutine = StartCoroutine(MovementCoroutine());
+    }
+    
+    public void StopMovement()
+    {
+        animator.SetTrigger("Idle");
+        if (_movementCoroutine != null)
+        {
+            StopCoroutine(_movementCoroutine);
+            _movementCoroutine = null;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator MovementCoroutine()
     {
-        
+        while (true)
+        {
+            if (_state == CivilianState.Idle)
+            {
+                _state = CivilianState.Moving;
+                agent.SetDestination(RandomNavmeshLocation(transform.position, 100f));
+            }
+            else if (_state == CivilianState.Moving)
+            {
+                if (agent.remainingDistance <= agent.stoppingDistance)
+                {
+                    _state = CivilianState.Idle;
+                }
+            }
+            else if (_state == CivilianState.Dead)
+            {
+                break;
+            }
+
+            yield return null;
+        }
+    }
+    
+    public Vector3 RandomNavmeshLocation(Vector3 point, float radius) {
+        Vector3 randomDirection = point + Random.insideUnitSphere * radius;
+        NavMeshHit hit;
+        Vector3 finalPosition = Vector3.zero;
+        if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1)) {
+            finalPosition = hit.position;            
+        }
+        return finalPosition;
     }
 }
---
+++ Assets/Game/Scripts/Behaviours/CivilianSpawnerBehaviour.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.FPS.Gameplay;
+using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
+namespace Game.Scripts.Behaviours
+{
+    public class CivilianSpawnerBehaviour : MonoBehaviour
+    {
+        [SerializeField] private List<CivilianBehaviour> civilianPrefabs;
+        [SerializeField] private int initialCivilianCount = 20;
+        [SerializeField] private int maxCivilianCount = 50;
+        [SerializeField] private int civilianSpawnInterval = 10;
+
+        private List<CivilianBehaviour> _spawnedCivilians;
+        private PlayerCharacterController _player;
+        private Coroutine _civilianSpawnerCoroutine;
+
+        private PlayerCharacterController Player
+        {
+            get
+            {
+                if(_player == null)
+                    _player = FindObjectOfType<PlayerCharacterController>();
+                return _player;
+            }
+            
+        }
+
+        private void Start()
+        {
+            _spawnedCivilians = new List<CivilianBehaviour>();
+            for (int i = 0; i < initialCivilianCount; i++)
+            {
+                CivilianBehaviour civil = SpawnRandomCivilian();
+                civil.StartMovement();
+                
+                _spawnedCivilians.Add(civil);
+            }
+            _civilianSpawnerCoroutine = StartCoroutine(CivilianSpawnerCoroutine());
+        }
+
+        private void OnDestroy()
+        {
+            if (_civilianSpawnerCoroutine != null)
+            {
+                StopCoroutine(_civilianSpawnerCoroutine);
+                _civilianSpawnerCoroutine = null;
+            }
+        }
+
+        private IEnumerator CivilianSpawnerCoroutine()
+        {
+            while (true)
+            {
+                if (_spawnedCivilians.Count < maxCivilianCount)
+                {
+                    CivilianBehaviour civil = SpawnRandomCivilian();
+                    civil.StartMovement();
+                    
+                    _spawnedCivilians.Add(civil);
+                }
+                
+                yield return new WaitForSeconds(civilianSpawnInterval);
+            }
+        }
+
+        private CivilianBehaviour SpawnRandomCivilian()
+        { 
+            var civilianPrefab = civilianPrefabs[Random.Range(0, civilianPrefabs.Count)];
+            var civilian = Instantiate(civilianPrefab, GetRandomPosAroundPlayer(), Quaternion.identity);
+            return civilian;
+        }
+
+        private Vector3 GetRandomPosAroundPlayer()
+        {
+            float minRadius = 20;
+            float maxRadius = 30;
+            return RandomNavmeshLocation(Player.transform.position, minRadius, maxRadius);
+        }
+        
+        public Vector3 RandomNavmeshLocation(Vector3 point, float minRadius, float maxRadius)
+        {
+            Vector3 randomDirection = RandomPointInAnnulus(point, minRadius, maxRadius);
+            NavMeshHit hit;
+            Vector3 finalPosition = Vector3.zero;
+            if (NavMesh.SamplePosition(randomDirection, out hit, maxRadius, 1)) {
+                finalPosition = hit.position;            
+            }
+            return finalPosition;
+        }
+        
+        private Vector2 RandomPointInAnnulus(Vector2 origin, float minRadius, float maxRadius){
+ 
+            var randomDirection = (Random.insideUnitCircle * origin).normalized;
+            var randomDistance = Random.Range(minRadius, maxRadius);
+            var point = origin + randomDirection * randomDistance;
+            return point;
+        }
+    }
+}
