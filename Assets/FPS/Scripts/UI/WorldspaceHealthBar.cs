5a2b7194a9df5274988a7a635c1867f609bca891
Cem Küçükhasanoğlu <c.kucukhasanoglu@matchingham.gs>
2024-01-28 13:56:17 +0300

    healthbar update

--- Assets/FPS/Scripts/UI/WorldspaceHealthBar.cs
+++ Assets/FPS/Scripts/UI/WorldspaceHealthBar.cs
@@ -36,11 +36,8 @@ namespace Unity.FPS.UI
 
             // rotate health bar to face the camera/player
             HealthBarPivot.LookAt(Camera.main.transform.position);
-
-            // hide health bar if needed
-            if (HideFullHealthBar)
-                HealthBarPivot.gameObject.SetActive(HealthBarImage.fillAmount != 1);
-            else if (HealthBarImage.fillAmount <= 0 && !_healthBarDisabling)
+            
+            if (HealthBarImage.fillAmount <= 0 && !_healthBarDisabling)
             {
                 _healthBarDisabling = true;
                 HealthBarImage.DOFade(0, 0.15f).SetEase(Ease.Linear).OnComplete(() =>
@@ -48,6 +45,10 @@ namespace Unity.FPS.UI
                     HealthBarImage.gameObject.SetActive(false);
                 });
             }
+            
+            // hide health bar if needed
+            if (HideFullHealthBar && !_healthBarDisabling)
+                HealthBarPivot.gameObject.SetActive(HealthBarImage.fillAmount != 1);
         }
     }
 }
